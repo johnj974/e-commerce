a1f0773d3332b46bbf31dedf8761fcd379fc6730
johnj974 <jjdug@hotmail.com>
2020-03-17 09:13:09 +0000

    connecting to heroku

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,6 +22,7 @@ from search import urls as urls_search          # import our search url to be us
 from django.views import static
 from .settings import MEDIA_ROOT
 from cart import urls as urls_cart
+from checkout import urls as urls_checkout
 
 
 urlpatterns = [
@@ -32,4 +33,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
     url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
+    url(r'^checkout/', include(urls_checkout)),
 ]
